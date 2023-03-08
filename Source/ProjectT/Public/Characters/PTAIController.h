1518f485332fd94f1cb5ee20cb5068630687af31
hsh5206 <hyunhsh5206@gmail.com>
2023-03-09 01:49:11 +0900

    AbilitySystem - DamageExeCalc Replicate

---
+++ Source/ProjectT/Public/Characters/PTAIController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "PTAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTT_API APTAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+
+};
